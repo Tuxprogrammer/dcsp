9bd30215393ee578b0886be736b57f9301980d8f
Spencer Callicott <tuxprogrammer@gmail.com>
2017-04-04 16:42:41 -0500

    added testing values for sample tables

---
+++ assets/sql/testing_groups.sql
@@ -0,0 +1,6 @@
+INSERT INTO sc2257.groups (groupName, groupDesc, gTimeStamp, gType, creator) VALUES ('asdf4', 'this', '2017-03-21 16:30:39', 1, 7);
+INSERT INTO sc2257.groups (groupName, groupDesc, gTimeStamp, gType, creator) VALUES ('asdf3', 'is', '2017-03-21 16:30:36', 1, 7);
+INSERT INTO sc2257.groups (groupName, groupDesc, gTimeStamp, gType, creator) VALUES ('asdf2', 'sparta', '2017-03-21 16:30:33', 1, 7);
+INSERT INTO sc2257.groups (groupName, groupDesc, gTimeStamp, gType, creator) VALUES ('asdf1', '!!!111!!!1!!11!', '2017-03-21 16:30:30', 1, 7);
+INSERT INTO sc2257.groups (groupName, groupDesc, gTimeStamp, gType, creator) VALUES ('asdfasdfasdf', '', '2017-04-04 16:14:07', 1, 7);
+INSERT INTO sc2257.groups (groupName, groupDesc, gTimeStamp, gType, creator) VALUES ('asdfasdfasdf', '', '2017-04-04 16:15:04', 1, 7);
