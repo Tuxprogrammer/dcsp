4140d649d079940b889877f7d593f6ca977d18a0
Spencer Callicott <tuxprogrammer@gmail.com>
2017-04-22 23:36:37 -0500

    Removed extra elseif branch.

--- assets/php/login.php
+++ assets/php/login.php
@@ -34,36 +34,38 @@ if (isset($_SESSION['userName'])) {
 
         $query = "SELECT * FROM users WHERE userName='$un_temp'";
         $result = $conn->query($query);
-        if (!$result) {die($conn->error);}
-        elseif ($result->num_rows) {
-            $result = $conn->query($query);
-            if (!$result) {
-                throw new Exception('Error authenticating user information. ' . $conn->error);
-            }
-
-            $result->data_seek(0);
-            $row = $result->fetch_array(MYSQLI_ASSOC);
-
-            $token = hash('sha512', "$pw_temp");
-
-            if ($token === $row['passwordHash']) {
-
-                $_SESSION['userName'] = $un_temp;
-                $_SESSION['realName'] = $row['realName'];
-                $_SESSION['userId'] = $row['userId'];
-                $_SESSION['admin'] = $row['admin'];
-                $_SESSION['banned'] = $row['banned'];
-                $_SESSION['avatarImage'] = $row['avatarImage'];
-                $_SESSION['phoneNumber'] = $row['phoneNumber'];
-                $_SESSION['emailAddress'] = $row['emailAddress'];
-
-                //echo "Hi ".$row['realName'].", you are now logged in as ".$row['username'];
-
-                header('Location: groups.php');
-            } else {
-                echo '<p>Invalid username/password combination</p>';
-            }
+        if (!$result) {
+            die($conn->error);
         }
+
+        $result = $conn->query($query);
+        if (!$result) {
+            throw new Exception('Error authenticating user information. ' . $conn->error);
+        }
+
+        $result->data_seek(0);
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+
+        $token = hash('sha512', "$pw_temp");
+
+        if ($token === $row['passwordHash']) {
+
+            $_SESSION['userName'] = $un_temp;
+            $_SESSION['realName'] = $row['realName'];
+            $_SESSION['userId'] = $row['userId'];
+            $_SESSION['admin'] = $row['admin'];
+            $_SESSION['banned'] = $row['banned'];
+            $_SESSION['avatarImage'] = $row['avatarImage'];
+            $_SESSION['phoneNumber'] = $row['phoneNumber'];
+            $_SESSION['emailAddress'] = $row['emailAddress'];
+
+            //echo "Hi ".$row['realName'].", you are now logged in as ".$row['username'];
+
+            header('Location: groups.php');
+        } else {
+            echo '<p>Invalid username/password combination</p>';
+        }
+
     }
     $conn->close();
 }
