12017cc6dccafdc9633b351a0918d114356bcd54
Spencer Callicott <tuxprogrammer@gmail.com>
2017-04-23 16:13:32 -0500

    Added dir variable

--- messages.php
+++ messages.php
@@ -40,12 +40,12 @@
 <div class="">
     <div class="container">
         <?php
-        require_once 'assets/php/get_messages.php';
-        require_once 'assets/php/send_message.php';
-        require_once 'assets/php/user_profile_list.php';
-        require_once 'assets/php/add_user.php';
-        require_once 'assets/php/remove_user.php';
-        require_once 'assets/php/user_profile_list.php';
+        require_once __DIR__.'/assets/php/get_messages.php';
+        require_once __DIR__.'/assets/php/send_message.php';
+        require_once __DIR__.'/assets/php/user_profile_list.php';
+        require_once __DIR__.'/assets/php/add_user.php';
+        require_once __DIR__.'/assets/php/remove_user.php';
+        require_once __DIR__.'/assets/php/user_profile_list.php';
         ?>
     </div>
 </div>
